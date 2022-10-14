d336e32138682a558faf8d526809ca300cf10643
YourDailyBlockchain <113318840+YourDailyBlockchain@users.noreply.github.com>
2022-10-13 21:52:34 -0700

    Add files via upload

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
 contract Inbox {
     string public message;
 
-    function Inbox(string initialMessage) public {
+    constructor (string memory initialMessage) {
         message = initialMessage;
     }
     function setMessage(string newMessage) public {
--- test/deploy.js
+++ test/deploy.js
@@ -1,26 +1,26 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
-
-const provider = new HDWalletProvider(
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
     'warm person floor inch assault harvest unit material soccer average code blind',
     'https://goerli.infura.io/v3/773a194525764d43a23d4119273ef851'
 );
 
 const web3 = new Web3(provider);
-
+ 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-
-    console.log('Attempting to deploy from account', accounts[0]);
-
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ gas: '1000000', from: accounts[0] });
-
-    console.log('Contract deployed to', result.options.address);
-
-    provider.engine.stop()
-
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
+ 
 deploy();
